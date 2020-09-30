c4f60744d6ca32ba235bab95a05b6fc36bc99797
DecoyTNT <42523466+DecoyTNT@users.noreply.github.com>
2020-09-29 23:04:37 -0500

    Cambio a la uri de producción

--- config/apollo.js
+++ config/apollo.js
@@ -3,8 +3,8 @@ import fetch from 'node-fetch';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-    // uri: 'https://crm-graphql-jm.herokuapp.com/',
-    uri: 'http://localhost:4000/',
+    uri: 'https://crm-graphql-jm.herokuapp.com/',
+    // uri: 'http://localhost:4000/',
     fetch
 });
 
